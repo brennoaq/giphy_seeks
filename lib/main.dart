27d363b6234df9900a53f0e30219136dc596f024
vek-brenno.queiroz <brenno.queiroz@vectorinf.com.br>
2020-01-23 18:13:33 -0300

    90% finished

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:giphy_seeks/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Homepage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
